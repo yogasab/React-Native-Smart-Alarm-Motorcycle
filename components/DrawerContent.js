bd003807b9986558ee1e1c507987bc4f0197d2e1
Yoga Baskoro <baskoroyoga40@gmail.com>
2021-05-14 14:10:03 +0700

    Install Firebase Push Notification apllied to MainAplication

--- components/DrawerContent.js
+++ components/DrawerContent.js
@@ -1,87 +1,87 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
-import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
-import {
-  Avatar,
-  Title,
-  Caption,
-  // Paragraph,
-  Drawer,
-  // Text,
-  // TouchableRipple,
-  // Switch,
-} from 'react-native-paper';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import ProfileSettings from './pages/ProfileScreen/ProfileSettings';
+// import React from 'react';
+// import {View, StyleSheet, Text} from 'react-native';
+// import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
+// import {
+//   Avatar,
+//   Title,
+//   Caption,
+//   // Paragraph,
+//   Drawer,
+//   // Text,
+//   // TouchableRipple,
+//   // Switch,
+// } from 'react-native-paper';
+// import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+// import ProfileSettings from './pages/ProfileScreen/ProfileSettings';
 
-const DrawerContent = props => {
-  return (
-    <View style={{flex: 1}}>
-      {/* <DrawerContentScrollView {...props}>
-        <View style={styles.drawerContent}>
-          <View style={styles.userInfoSection}>
-            <View style={{flexDirection: 'row', marginTop: 2}}>
-              <Avatar.Image
-                source={require('../assets/images/avatar.jpg')}
-                size={50}
-              />
-              <View style={{marginLeft: 13, marginTop: -2}}>
-                <Title style={styles.title}>Yoga Baskoro</Title>
-                <Caption style={styles.caption}>Honda CBR 250RR</Caption>
-              </View>
-            </View>
-          </View>
-          <Drawer.Section style={styles.drawerSection}>
-            <DrawerItem
-              icon={({color, size}) => (
-                <Icon name="account" color={color} size={size} />
-              )}
-              label="Profile"
-              onPress={() => {
-                props.navigation.navigate('ProfileSettings');
-              }}
-            />
-          </Drawer.Section>
-        </View>
-      </DrawerContentScrollView> */}
-      <Drawer.Section style={styles.bottomDrawerSection}>
-        <DrawerItem
-          icon={({color, size}) => (
-            <Icon name="information" color={color} size={size} />
-          )}
-          label="About"
-          onPress={() => alert('Made with love by yogasab')}
-        />
-      </Drawer.Section>
-    </View>
-  );
-};
+// const DrawerContent = props => {
+//   return (
+//     <View style={{flex: 1}}>
+//       {/* <DrawerContentScrollView {...props}>
+//         <View style={styles.drawerContent}>
+//           <View style={styles.userInfoSection}>
+//             <View style={{flexDirection: 'row', marginTop: 2}}>
+//               <Avatar.Image
+//                 source={require('../assets/images/avatar.jpg')}
+//                 size={50}
+//               />
+//               <View style={{marginLeft: 13, marginTop: -2}}>
+//                 <Title style={styles.title}>Yoga Baskoro</Title>
+//                 <Caption style={styles.caption}>Honda CBR 250RR</Caption>
+//               </View>
+//             </View>
+//           </View>
+//           <Drawer.Section style={styles.drawerSection}>
+//             <DrawerItem
+//               icon={({color, size}) => (
+//                 <Icon name="account" color={color} size={size} />
+//               )}
+//               label="Profile"
+//               onPress={() => {
+//                 props.navigation.navigate('ProfileSettings');
+//               }}
+//             />
+//           </Drawer.Section>
+//         </View>
+//       </DrawerContentScrollView> */}
+//       <Drawer.Section style={styles.bottomDrawerSection}>
+//         <DrawerItem
+//           icon={({color, size}) => (
+//             <Icon name="information" color={color} size={size} />
+//           )}
+//           label="About"
+//           onPress={() => alert('Made with love by yogasab')}
+//         />
+//       </Drawer.Section>
+//     </View>
+//   );
+// };
 
-export default DrawerContent;
+// export default DrawerContent;
 
-const styles = StyleSheet.create({
-  bottomDrawerSection: {
-    borderTopColor: '#f4f4f4',
-    borderTopWidth: 4,
-  },
-  userInfoSection: {
-    paddingLeft: 15,
-    paddingTop: 13,
-  },
-  drawerContent: {
-    flex: 1,
-  },
-  drawerSection: {
-    marginTop: 15,
-    borderBottomWidth: 0,
-  },
-  title: {
-    fontSize: 18,
-    marginTop: 3,
-    fontFamily: 'OpenSans-Bold',
-  },
-  caption: {
-    fontSize: 14,
-    lineHeight: 14,
-  },
-});
+// const styles = StyleSheet.create({
+//   bottomDrawerSection: {
+//     borderTopColor: '#f4f4f4',
+//     borderTopWidth: 4,
+//   },
+//   userInfoSection: {
+//     paddingLeft: 15,
+//     paddingTop: 13,
+//   },
+//   drawerContent: {
+//     flex: 1,
+//   },
+//   drawerSection: {
+//     marginTop: 15,
+//     borderBottomWidth: 0,
+//   },
+//   title: {
+//     fontSize: 18,
+//     marginTop: 3,
+//     fontFamily: 'OpenSans-Bold',
+//   },
+//   caption: {
+//     fontSize: 14,
+//     lineHeight: 14,
+//   },
+// });
--- components/pages/SettingsScreen/AboutSetting.js
+++ components/pages/SettingsScreen/AboutSetting.js
@@ -1,69 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {Image, StyleSheet, Text, View, Linking, Button} from 'react-native';
-import firestore from '@react-native-firebase/firestore';
+// import firestore from '@react-native-firebase/firestore';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import RNLocation from 'react-native-location';
 
-RNLocation.configure({
-  distanceFilter: 401,
-});
 
 const AboutSetting = () => {
-  // const [lokasi, setLokasi] = useState([]);
-  // const permissionHandle = async () => {
-  //   let permission = await RNLocation.checkPermission({
-  //     ios: 'whenInUse',
-  //     android: {
-  //       detail: 'coarse',
-  //     },
-  //   });
-  //   // console.log('Permission ', permission);
-
-  //   let location;
-  //   if (!permission) {
-  //     permission = await RNLocation.requestPermission({
-  //       ios: 'whenInUse',
-  //       android: {
-  //         detail: 'coarse',
-  //         rationale: {
-  //           title: 'We need to access your location',
-  //           message:
-  //             'We use your location to match location between the motorcycle and yours',
-  //           buttonPositive: 'OK',
-  //           buttonNegative: 'Cancel',
-  //         },
-  //       },
-  //     });
-  //     // console.log('Permission 2 ', permission);
-  //     location = await RNLocation.getLatestLocation({timeout: 100});
-  //     // console.log(
-  //     //   location,
-  //     //   location.longitude,
-  //     //   location.latitude,
-  //     //   location.timestamp,
-  //     // );
-  //     setLokasi(location);
-  //   } else {
-  //     // console.log('Else');
-  //     location = await RNLocation.getLatestLocation({timeout: 100});
-  //     // console.log(
-  //     //   location,
-  //     //   location.longitude,
-  //     //   location.latitude,
-  //     //   location.timestamp,
-  //     // );
-  //     setLokasi(location);
-  //   }
-  // };
-  const getLocation = async () => {
-    let location = await RNLocation.getLatestLocation({timeout: 100});
-    console.log(location.latitude);
-    console.log(location.longitude);
-  };
-  useEffect(() => {
-    getLocation();
-  }, []);
-
   return (
     <View style={styles.container}>
       <View style={styles.logoWrapper}>
@@ -76,18 +17,6 @@ const AboutSetting = () => {
         <Text style={styles.aboutText}>About</Text>
       </View>
       <View style={styles.textWrapper}>
-        {/* <View style={{margin: 10, padding: 20}}>
-          <Button
-            title="Get Location"
-            color="#009387"
-            onPress={permissionHandle}
-          />
-        </View>
-        <View>
-          <Text>{lokasi.latitude}</Text>
-          <Text>{lokasi.longitude}</Text>
-        </View> */}
-
         <Text
           onPress={() => Linking.openURL('https://github.com/yogasab')}
           style={{fontSize: 17}}>
